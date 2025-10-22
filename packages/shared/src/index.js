dbd23a6487aa6a6a27f07d218eddeb8b0fbcacc4
jbax1899 <jbax1899@gmail.com>
2025-10-22 17:50:13 -0400

    fix import links between packages

--- packages/shared/src/index.js
+++ packages/shared/src/index.js
@@ -4,4 +4,8 @@
  * registry utilities can evolve without churn in downstream imports.
  */
 export { PromptRegistry, getActivePromptRegistry, renderPrompt, setActivePromptRegistry } from './prompts/promptRegistry.js';
+/**
+ * Trace storage utilities for persisting and retrieving response metadata.
+ */
+export { TraceStore, createTraceStore, defaultTraceStore } from './traceStore.js';
 //# sourceMappingURL=index.js.map
